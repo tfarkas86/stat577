2ffcf7b5803ab721269ea7f13790f050b326f1e6
Tim Farkas <timothy.farkas@gmail.com>
2020-05-14 09:02:33 -0600

    tons

--- project/code/print_figures.R
+++ project/code/print_figures.R
@@ -3,25 +3,25 @@
 
 # betacarotene over time model
 pdf("Dropbox/stat577/project/figures/bc_overtime_trace.pdf")
-traceplot(fit_bc, ask = FALSE, mfrow = c(3, 3), 
+traceplot(fit_bc1, ask = FALSE, mfrow = c(3, 3), 
           width = 5000, height= 5000)
 dev.off()
 
 
-# betacarotene interaction model
-pdf("Dropbox/stat577/project/figures/bc_ve_trace.pdf")
+# vitmain E dose model
+pdf("Dropbox/stat577/project/figures/ve_trace.pdf")
+traceplot(fit_ve1, ask = FALSE, mfrow = c(3, 3), 
+          width = 5000, height= 5000)
+dev.off()
+
+# vitamin E correlaton model
+pdf("Dropbox/stat577/project/figures/ve_corr_trace.pdf")
 traceplot(fit_cor, ask = FALSE, mfrow = c(3, 3), 
           width = 5000, height= 5000)
 dev.off()
 
-# vitamin E  model
-pdf("Dropbox/stat577/project/figures/ve_trace.pdf")
-traceplot(fit_ve, ask = FALSE, mfrow = c(3, 3), 
-          width = 5000, height= 5000)
-dev.off()
-
-# betacarotene - vitamin E correlation model
+# betacarotene interaction model
 pdf("Dropbox/stat577/project/figures/bc_int_trace.pdf")
-traceplot(fit_bc_int, ask = FALSE, mfrow = c(3, 3), 
+traceplot(fit_bc_int1, ask = FALSE, mfrow = c(3, 3), 
           width = 5000, height= 5000)
 dev.off()

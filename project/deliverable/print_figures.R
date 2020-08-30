dd8cf05ebf091760f51a378744668e309e2b1727
Tim Farkas <timothy.farkas@gmail.com>
2020-08-30 10:54:52 -0600

    all final project deliverables

--- project/deliverable/print_figures.R
+++ project/deliverable/print_figures.R
@@ -2,26 +2,26 @@
 # Cuz R Notebooks not working
 
 # betacarotene over time model
-png("Dropbox/stat577/project/figures/bc_overtime_trace.png")
+pdf("Dropbox/stat577/project/figures/bc_overtime_trace.pdf")
 traceplot(fit_bc1, ask = FALSE, mfrow = c(3, 3), 
           width = 5000, height= 5000)
 dev.off()
 
 
 # vitmain E dose model
-png("Dropbox/stat577/project/figures/ve_trace.png")
+pdf("Dropbox/stat577/project/figures/ve_trace.pdf")
 traceplot(fit_ve1, ask = FALSE, mfrow = c(3, 3), 
           width = 5000, height= 5000)
 dev.off()
 
 # vitamin E correlaton model
-png("Dropbox/stat577/project/figures/ve_corr_trace.png")
-traceplot(fit_cor1, ask = FALSE, mfrow = c(3, 3), 
+pdf("Dropbox/stat577/project/figures/ve_corr_trace.pdf")
+traceplot(fit_cor, ask = FALSE, mfrow = c(3, 3), 
           width = 5000, height= 5000)
 dev.off()
 
 # betacarotene interaction model
-png("Dropbox/stat577/project/figures/bc_int_trace.png")
+pdf("Dropbox/stat577/project/figures/bc_int_trace.pdf")
 traceplot(fit_bc_int1, ask = FALSE, mfrow = c(3, 3), 
           width = 5000, height= 5000)
 dev.off()
